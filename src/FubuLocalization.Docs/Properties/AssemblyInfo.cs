e5b7adb266d81186132d2f68d9ad7495c134938d
jeremydmiller <jeremydmiller@yahoo.com>
2013-04-22 15:24:05 -0500

    cleaned up after the docs changes

--- src/FubuLocalization.Docs/Properties/AssemblyInfo.cs
+++ src/FubuLocalization.Docs/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FubuLocalization.Docs")]
-[assembly: FubuDocModule("localization")]
+
